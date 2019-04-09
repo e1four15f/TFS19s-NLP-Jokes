08b6f856dfb2b26d61bc6d7deb7efc15e132b3bc
muhamob <yusov.aa@phystech.edu>
2019-04-09 14:53:10 +0300

    поправил .gitignore

--- bash_parser.py
+++ bash_parser.py
@@ -1,5 +1,6 @@
+
 from src.parser import bash_parser as bp
 
 
-p = bp.BashParser(list(range(0, 2923)), db='jokes/jokes.db')
+p = bp.BashParser(list(range(0, 2923)), db='data/jokes.db')
 p.parse(10)
